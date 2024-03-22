2e7e72505bf0617fab7e5171409b5893d75ec8dc
sanderpiva <84524010+sanderpiva@users.noreply.github.com>
2024-03-22 10:05:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,48 @@
 const URL = "./my_model/";
-let model, webcam, ctx, labelContainer, maxPredictions;
 
-async function init() {
-    const modelURL = URL + "model.json";
-    const metadataURL = URL + "metadata.json";
+    let model, webcam, labelContainer, maxPredictions;
 
-    // load the model and metadata
-    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-    // Note: the pose library adds a tmPose object to your window (window.tmPose)
-    model = await tmPose.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
+    // Load the image model and setup the webcam
+    async function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
 
-    // Convenience function to setup a webcam
-    const size = 200;
-    const flip = true; // whether to flip the webcam
-    webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
-    await webcam.setup(); // request access to the webcam
-    await webcam.play();
-    window.requestAnimationFrame(loop);
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // or files from your local hard drive
+        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        model = await tmImage.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
 
-    // append/get elements to the DOM
-    const canvas = document.getElementById("canvas");
-    canvas.width = size; canvas.height = size;
-    ctx = canvas.getContext("2d");
-    labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement("div"));
+        // Convenience function to setup a webcam
+        const flip = true; // whether to flip the webcam
+        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append elements to the DOM
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
+        labelContainer = document.getElementById("label-container");
+        //for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        //}
+
+
     }
-}
 
-async function loop(timestamp) {
-    webcam.update(); // update the webcam frame
-    await predict();
-    window.requestAnimationFrame(loop);
-}
+    async function loop() {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
 
-async function predict() {
-    // Prediction #1: run input through posenet
-    // estimatePose can take in an image, video or canvas html element
-    const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
-    // Prediction 2: run input through teachable machine classification model
-    const prediction = await model.predict(posenetOutput);
+    // run the webcam image through the image model
+    async function predict() {
+        // predict can take in an image, video or canvas html element
+        const prediction = await model.predict(webcam.canvas);
 
-    /*for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
-    }*/
-
-    console.log(prediction);
+        console.log(prediction);
 
         let maxProb=0;
         let maxName ="";
@@ -63,47 +57,6 @@ async function predict() {
         const classPrediction =
                 maxName + ": " + maxProb.toFixed(2);
             labelContainer.childNodes[0].innerHTML = classPrediction;
-            
-        if (maxName=="cima"){
-            acao.innerHTML = "levantar cama";
-            
-        }else if(maxName=="esquerda"){
-            acao.innerHTML = "ir ao banheiro";
-            
-        }else if(maxName=="direita"){
-            acao.innerHTML = "deitar cama";
-            
-        }else if(maxName=="baxio"){
-            acao.innerHTML = "deitar cama";
-            
-        }else if(maxName=="none"){
-            acao.innerHTML = "sem ação";
-            
-        }    
-            //acao.innerHTML = "levantar cama";
-            //acao.innerHTML = "deitar cama";
-            //acao.innerHTML = "comer";
-            //acao.innerHTML = "ir ao banheiro";
-            
-            
+    
         
-
-        
-    // finally draw the poses
-    drawPose(pose);
-}
-
-function drawPose(pose) {
-    if (webcam.canvas) {
-        ctx.drawImage(webcam.canvas, 0, 0);
-        // draw the keypoints and skeleton
-        if (pose) {
-            const minPartConfidence = 0.5;
-            tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
-            tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
-        }
     }
-}
-
-
-        
